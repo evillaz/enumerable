940a318b1749164b4657c7bcb3d835a247462dbf
evillaz <evillaz123@gmail.com>
2023-05-26 04:21:09 -0500

    Update my_enumerable.rb

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,15 +1,15 @@
 module MyEnumerable
-    def all?
-      each { |item| return puts(false) unless yield(item) }
-      puts(true)
-    end
-  
-    def any?
-      each { |item| return puts(true) if yield(item) }
-      puts(false)
-    end
+  def all?
+    each { |item| return puts(false) unless yield(item) }
+    puts(true)
+  end
 
-def filter
+  def any?
+    each { |item| return puts(true) if yield(item) }
+    puts(false)
+  end
+
+  def filter
     result = []
     each do |item|
       result << item if yield(item)
