d754f7d22fc595c412d819593bfee6393949d84d
evillaz <evillaz123@gmail.com>
2023-05-26 04:10:34 -0500

    Create my list class

---
+++ my_list.rb
@@ -0,0 +1,20 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(list)
+    @list = list
+    puts "#MyList: @list=#{@list.inspect}>"
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+list = MyList.new([1, 2, 3, 4])
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+list.filter(&:even?)
